f36201365d0c1bcf955833e1e1b4d709e5d2e678
Kishor Davara <kkd11993@gmail.com>
2025-06-14 14:39:32 +0530

    added product controller to create the product

---
+++ product-service/src/main/java/com/microservices/product/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.microservices.product.controller;
+
+import com.microservices.product.dto.ProductRequest;
+import com.microservices.product.model.Product;
+import com.microservices.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product createProduct(@RequestBody ProductRequest productRequest){
+        return productService.createProduct(productRequest);
+    }
+}
