8668082e0343d87b0a94ef5582c8ad7a35c89772
Kishor Davara <kkd11993@gmail.com>
2025-06-18 19:19:50 +0530

    integration testsuite for fetch orders api

--- order-service/src/test/java/com/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/microservices/order/OrderServiceApplicationTests.java
@@ -5,13 +5,18 @@ import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
 import static com.microservices.order.TestcontainersConfiguration.mysqlContainer;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 
 @Import(TestcontainersConfiguration.class)
@@ -37,6 +42,7 @@ class OrderServiceApplicationTests {
     }
 
     @Test
+    @Order(1)
     void shouldPlaceOrder() {
         String requestBody = """
                 {
@@ -58,5 +64,22 @@ class OrderServiceApplicationTests {
                 .body("skuCode", equalTo("iphone_15"))
                 .body("price", equalTo(1000))
                 .body("quantity", equalTo(2));
+    }
+
+    @Test
+    @Order(2)
+    void shouldFetchOrders() {
+        RestAssured
+                .given()
+                .when()
+                .get("/api/orders")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("$", not(empty()))
+                .body("id", everyItem(notNullValue()))
+                .body("orderNumber", everyItem(notNullValue()))
+                .body("skuCode", hasItem("iphone_15"))
+                .body("find { it.skuCode == 'iphone_15' }.price", equalTo(1000.0F))
+                .body("find { it.skuCode == 'iphone_15' }.quantity", equalTo(2));
     }
 }
