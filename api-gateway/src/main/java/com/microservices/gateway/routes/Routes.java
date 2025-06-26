ecc07d222d766d51443a5718922a600dab94e57c
Kishor Davara <kkd11993@gmail.com>
2025-06-26 21:44:46 +0530

    order service route changes

--- api-gateway/src/main/java/com/microservices/gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/microservices/gateway/routes/Routes.java
@@ -17,4 +17,11 @@ public class Routes {
                 .route(RequestPredicates.path("/api/products"), HandlerFunctions.http("http://localhost:8080"))
                 .build();
     }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoute() {
+        return GatewayRouterFunctions.route("order_service")
+                .route(RequestPredicates.path("/api/orders"), HandlerFunctions.http("http://localhost:8081"))
+                .build();
+    }
 }
